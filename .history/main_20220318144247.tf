377d2ace9014a5d9b91e11d39bb2ba11161ad7f0
JEFFREY-CHIJIOKE-UCHE <JEFFREY.CHIJIOKE-UCHE@ibm.com>
2022-03-18 14:43:08 -0400

    Updated

--- .history/main_20220318144247.tf
+++ .history/main_20220318144247.tf
@@ -15,14 +15,10 @@ resource "random_id" "buc" {
 
 resource "aws_s3_bucket" "buc" {
   bucket = "test-s3-${random_id.buc.hex}"
+  acl    = "private"
 
   tags = {
     Name        = "Test S3 Purpose"
     Environment = "Testing My AWS Access"
   }
-}
-
-resource "aws_s3_bucket_acl" "buc" {
-  bucket = aws_s3_bucket.buc.id
-  acl    = "private"
 }
